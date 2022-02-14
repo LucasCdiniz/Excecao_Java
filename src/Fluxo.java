06178a75856ca81f107db7f0a72dac6ed0557d8d
Lucas Diniz <lucascelestino.diniz@gmail.com>
2022-02-14 10:08:59 -0300

    Introdução a exceções

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -2,34 +2,15 @@
 public class Fluxo {
 
 	public static void main (String [] args) {
-		System.out.println("Ini do main");
-		try {
-			metodo1();
-		}catch(ArithmeticException | NullPointerException ex) {
-			String msg = ex.getMessage();
-			System.out.println("Exception: " + msg);
-			ex.printStackTrace();
-		}
-		System.out.println("Fim do main");
-	}
-	
-	private static void metodo1() {
-		System.out.println("Ini do metodo1");
-		metodo2();
-		System.out.println("Fim do metodo1");
-	}
-	
-	private static void metodo2() {
-		System.out.println("Ini do metodo2");
-		for(int i = 1; i <= 5; i++) {
-			System.out.println(i);
+		String nome = "Alura";
+        System.out.println("ANTES");
 
-			//Primeira exceção: int a = i/0;
-			
-			Conta c = null;
-			c.deposita();
-			
-		}
-		System.out.println("Fim do metodo2");
+        try {
+            System.out.println(nome);
+        } catch(ArrayIndexOutOfBoundsException ex) {
+            System.out.println("CATCH");
+        }
+
+        System.out.println("DEPOIS");
 	}
 }
